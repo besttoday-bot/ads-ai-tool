1eb77df9ea27d6a66c7fe33e94b0fd69588c2331
Shiny Ikejiri <shiny_i@ShinynoMacBook-Pro.local>
2026-05-28 10:02:48 +0900

    use supabase service role key

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const openai = new OpenAI({
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
 async function getAccessToken() {
